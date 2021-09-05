5921aea7d0446d0e87da71e50709236d62c7f51d
JorgeDevOps-GitHub <jorgeDevOps20@gmail.com>
2021-09-05 18:25:49 -0500

    Updates

---
+++ precache-manifest.f105904812b5bbc40c8fa883fb5726d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "27637db95880a84c0811",
+    "url": "/static/css/main.f3e80e7b.chunk.css"
+  },
+  {
+    "revision": "27637db95880a84c0811",
+    "url": "/static/js/main.27637db9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bb382f76840fa801dcf2",
+    "url": "/static/css/2.bd1bc97f.chunk.css"
+  },
+  {
+    "revision": "bb382f76840fa801dcf2",
+    "url": "/static/js/2.bb382f76.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/logo.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logoNav.0026b862.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "4eb802522480d27333e732cfc3703980",
+    "url": "/index.html"
+  }
+];
